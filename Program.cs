c6f07f6300f586254161f962193710d9cac8c466
Henrique <henriquebk20@gmail.com>
2022-01-19 17:36:59 -0300

    Desafio do Xenlongao

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Teste_de_desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testes; i++)
+            {
+                var esferas = int.Parse(Console.ReadLine());
+                Console.WriteLine(esferas - Math.Floor(Math.Sqrt(esferas)));
+            }
+        }
+    }
+}
